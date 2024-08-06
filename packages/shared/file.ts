5eccd1dc0384e5e78361eac8643637219f7c66e9
任振鹏 <1074720760@qq.com>
2024-08-06 09:37:41 +0800

    feat(@nubuild/shared): 添加copy方法

--- packages/shared/file.ts
+++ packages/shared/file.ts
@@ -25,3 +25,36 @@ export const rm = async (file: string) => {
     return false
   }
 }
+/**
+ * 复制目录文件
+ * @param src 目标目录路径
+ * @param dest 要复制到的目录路径
+ * @returns 返回复制文件的路径数组
+ */
+export const copy = async (src: string, dest: string) => {
+  const stat = await fs.stat(src)
+  let files: string[] = []
+  if (stat.isDirectory()) {
+    files = await fs.readdir(src)
+  }
+
+  const filePaths = files.map((file: string) => {
+    const srcDir = `${src}/${file}`
+    const destDir = `${dest}/${src}/${file}`
+
+    return {
+      src: srcDir,
+      fullSrc: resolve(srcDir),
+      dest: destDir,
+      fullDest: resolve(destDir),
+    }
+  })
+
+  for (const file of filePaths) {
+    const fileTarget = Bun.file(file.fullSrc)
+    const fileContent = await fileTarget.text()
+    await Bun.write(file.fullDest, fileContent)
+  }
+
+  return filePaths.map((file) => file.dest)
+}
